881e444f22b7085511a9739c00117b7aae516290
a5rar <asra.hussain@digital.justice.gov.uk>
2020-01-08 15:17:55 +0000

    Checking in spring boot appp initialised

---
+++ src/main/java/com/port60/forntend/VdsClientFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.port60.forntend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdsClientFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdsClientFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/port60/forntend/VdsClientFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.port60.forntend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VdsClientFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
